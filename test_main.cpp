d7ff45a3318ec30237f10b0792479a73d24da986
cosminpopovici14 <161360825+cosminpopovici14@users.noreply.github.com>
2024-07-30 17:56:18 +0300

    Update test_main.cpp

--- test_main.cpp
+++ test_main.cpp
@@ -1,7 +1,14 @@
 #include <gtest/gtest.h>
 
-TEST(SampleTest, Test1) {
-    EXPECT_EQ(1 + 1, 2);
+// Funcție simplă de testat
+int add(int a, int b) {
+    return a + b;
+}
+
+// Test pentru funcția add
+TEST(AdditionTest, PositiveNumbers) {
+    EXPECT_EQ(add(1, 2), 3);
+    EXPECT_EQ(add(10, 20), 30);
 }
 
 int main(int argc, char **argv) {
